7d39a7b62ddbf70c22f53df1fd9c05491182fbd0
Anton <anton-flash@ukr.net>
2025-02-10 22:28:10 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,8 +5,8 @@ from app.main import check_password
 @pytest.mark.parametrize(
     "password, results",
     [
-        ("Valid1@", True),
-        ("ValidPassword123@", True),
+        ("Valid1@", False),
+        ("ValidPassword123@", False),
         ("short1@", False),
         ("VeryLongPassword123@", False),
         ("12345678", False),
@@ -15,8 +15,13 @@ from app.main import check_password
         ("1@Short", False),
         ("Valid@123", True),
         ("NoSpecialChar1", False),
-        ("NoUppercase1@", False),
-        ("Valid1#password", False),
+        ("NoUppercase1@", True),
+        ("Valid1#password", True),
+        ("lowercase1@", False),
+        ("uppercase@1", False),
+        ("1234@abcD", True),
+        ("wamdomawodma", False),
+        (" ", False)
     ]
 )
 def test_check_password(
